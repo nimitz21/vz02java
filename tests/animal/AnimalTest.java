2bd78753c23011e4d21d885bc605099970c69ab6
Reinaldo Ignatius <rei_iw@yahoo.com>
2017-03-28 18:55:03 +0700

    Ctor manual zoo dibenerin dikit

--- tests/animal/AnimalTest.java
+++ tests/animal/AnimalTest.java
@@ -45,7 +45,15 @@ public class AnimalTest extends TestCase {
   }
 
   public void testMove() throws Exception {
-
+		Animal a= new Animal ("WF", new Pair(0, 0));
+		a.Move(0);
+		assert(a.GetPos().first == -1 && a.GetPos().second == 0);
+		a.Move(1);
+		assert(a.GetPos().first == -1 && a.GetPos().second == -1);
+		a.Move(2);
+		assert(a.GetPos().first == -1 && a.GetPos().second == 0);
+		a.Move(3);
+		assert(a.GetPos().first == 0 && a.GetPos().second == 0);
   }
 
 }
